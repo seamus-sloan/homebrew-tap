1e55e57170cec225c85ff870fa0108cddfb6cb2b
ssloanRIIS <ssloan@riis.com>
2024-11-06 20:42:35 -0500

    update version number again

--- Formula/ss3.rb
+++ Formula/ss3.rb
@@ -1,7 +1,7 @@
 class Ss3 < Formula
   desc "Interactive AWS S3 CLI tool"
   homepage "https://github.com/seamus-sloan/aws-s3-bucket-tool"
-  url "https://github.com/seamus-sloan/aws-s3-bucket-tool/archive/refs/tags/v1.0.0.tar.gz" # v1.0.0
+  url "https://github.com/seamus-sloan/aws-s3-bucket-tool/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "68910c30ea9adcc267a7057617637de12eb42409b93d9d20fd6e4869d96fc8b0"
   license "MIT"
 

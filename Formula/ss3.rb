3fd6af23d61801853756592a2062d1a6817ae708
ssloanRIIS <ssloan@riis.com>
2024-11-06 20:13:55 -0500

    update ss3 (v1.0.1)

--- Formula/ss3.rb
+++ Formula/ss3.rb
@@ -1,16 +1,19 @@
 class Ss3 < Formula
   desc "Interactive AWS S3 CLI tool"
   homepage "https://github.com/seamus-sloan/aws-s3-bucket-tool" # Link to the main repo or homepage for ss3
-  url "https://github.com/seamus-sloan/aws-s3-bucket-tool/archive/refs/tags/v1.0.0.tar.gz" # Link to a tagged release
-  sha256 "e21fe5ccf6c586ce8e6b4a0fb9f8c41c5375caef509d16e74d79d45835d39cbd" # SHA256 of the release archive
+  url "https://github.com/seamus-sloan/aws-s3-bucket-tool/archive/refs/tags/v1.0.1.tar.gz" # Link to a tagged release
+  sha256 "61cc719eb0340febcf769907e8b5274497c554c964c07bcc76b9f276f6a7f656" # SHA256 of the release archive
   license "MIT"
 
   depends_on "ruby" # Specify Ruby as a dependency
 
 
   def install
-    # Install the main executable script
-    bin.install "aws-s3-bucket-tool.rb" => "ss3"
+    # Install all files to libexec
+    libexec.install Dir["*"]
+
+    # Create an executable in bin that points to the main script in libexec
+    (bin/"ss3").write_env_script libexec/"aws-s3-bucket-tool.rb", RUBYLIB: libexec
   end
 
   test do

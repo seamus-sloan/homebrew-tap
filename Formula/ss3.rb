f8d9ac73aa4b0b5b8c92c26e9aecc123223cd5d7
ssloanRIIS <ssloan@riis.com>
2024-11-06 20:35:09 -0500

    update SHA again for ss3 (v1.0.0)

--- Formula/ss3.rb
+++ Formula/ss3.rb
@@ -1,18 +1,21 @@
 class Ss3 < Formula
   desc "Interactive AWS S3 CLI tool"
-  homepage "https://github.com/seamus-sloan/aws-s3-bucket-tool" # Link to the main repo or homepage for ss3
-  url "https://github.com/seamus-sloan/aws-s3-bucket-tool/archive/refs/tags/v1.0.1.tar.gz" # Link to a tagged release
-  sha256 "61cc719eb0340febcf769907e8b5274497c554c964c07bcc76b9f276f6a7f656" # SHA256 of the release archive
+  homepage "https://github.com/seamus-sloan/aws-s3-bucket-tool"
+  url "https://github.com/seamus-sloan/aws-s3-bucket-tool/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "68910c30ea9adcc267a7057617637de12eb42409b93d9d20fd6e4869d96fc8b0"
   license "MIT"
 
-  depends_on "ruby" # Specify Ruby as a dependency
+  depends_on "ruby"
 
   def install
-    # Install all files to libexec
-    libexec.install Dir["*"]
+    # Install the main script
+    bin.install "aws-s3-bucket-tool.rb"
 
-    # Create an executable in bin that points to the main script in libexec
-    (bin/"ss3").write_env_script libexec/"aws-s3-bucket-tool.rb", RUBYLIB: libexec
+    # Install helper files to libexec
+    libexec.install "ui-helper.rb", "s3-helper.rb"
+
+    # Set AWS_S3_BUCKET_TOOL_LIB environment variable in the executable script
+    (bin/"aws-s3-bucket-tool").write_env_script libexec/"aws-s3-bucket-tool.rb", AWS_S3_BUCKET_TOOL_LIB: libexec
   end
 
   test do
